8c5349fd3890d74b10ddc04fb4cd13f03d328343
SU-RU <oooaa0727@gmail.com>
2017-10-21 18:06:56 +0800

    Eloquent 關聯

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,10 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+}
+$user = User::find(1);
+
+foreach ($user->tasks as $task) {
+    echo $task->name;
 }
