aac61715cf0c6646fbfb0239c0bf0acb2f10a41e
SU-RU <oooaa0727@gmail.com>
2017-10-21 20:56:51 +0800

    連接 Task 模型與 TaskPolicy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use App\Policies\TaskPolicy;
+use App\Task;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -12,8 +14,11 @@ class AuthServiceProvider extends ServiceProvider
      *
      * @var array
      */
+
+
     protected $policies = [
         'App\Model' => 'App\Policies\ModelPolicy',
+        Task::class => TaskPolicy::class,
     ];
 
     /**
